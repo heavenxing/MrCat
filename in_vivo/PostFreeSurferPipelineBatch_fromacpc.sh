faaef306fdfdb5d175393becf59182f72848d3ae
neuroecology <r.mars@donders.ru.nl>
2016-08-05 22:09:58 +0100

    WIP: Added HCP scripts

---
+++ in_vivo/PostFreeSurferPipelineBatch_fromacpc.sh
@@ -0,0 +1,111 @@
+#!/usr/bin/env bash
+set -e
+
+# set defaults
+args=""
+BatchFolder=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) # folder where this script is stored
+EnvironmentScript="${BatchFolder}/SetUpHCPPipeline.sh" # Pipeline environment script
+StudyFolder="${HOME}/projects/Pipelines_ExampleData" # Location of subject folders (named by subject IDs in SubjList)
+SubjList="100307" # Space delimited list of subject IDs
+LogDir="./log"
+runlocal="FALSE"
+
+# parse the input arguments
+for a in "$@" ; do
+  case $a in
+    --StudyFolder=*)  StudyFolder="${a#*=}"; shift ;;
+    --SubjList=*)     SubjList="${a#*=}"; shift ;;
+    --LogDir=*)       LogDir="${a#*=}"; shift ;;
+    --runlocal)       runlocal="TRUE"; shift ;;
+    *)                args="$args $a"; shift ;; # unsupported argument
+  esac
+done
+
+# check if no redundant arguments have been set
+if [[ -n $args ]] ; then
+  >&2 echo ""; >&2 echo "unsupported arguments are given:" $args
+  exit 1
+fi
+
+# Requirements for this script
+#  installed versions of: FSL (version 5.0.6), FreeSurfer (version 5.3.0-HCP), gradunwarp (HCP version 1.0.2) if doing gradient distortion correction
+#  environment: FSLDIR , FREESURFER_HOME , HCPPIPEDIR , CARET7DIR , PATH (for gradient_unwarp.py)
+
+# Set up pipeline environment variables and software
+. ${EnvironmentScript}
+
+# Log the originating call
+echo "$@"
+
+#if [[ -n $SGE_ROOT ]] ; then
+    QUEUE="-q long.q"
+    #QUEUE="-q hcp_priority.q"
+#fi
+
+PRINTCOM=""
+#PRINTCOM="echo"
+
+# set the cluster queuing or local execution command
+if [[ $runlocal == TRUE ]] ; then
+    echo "About to run ${HCPPIPEDIR}/PreFreeSurfer/PreFreeSurferPipeline.sh"
+    queuing_command=""
+else
+    echo "About to use fsl_sub to queue or run ${HCPPIPEDIR}/PreFreeSurfer/PreFreeSurferPipeline.sh"
+    mkdir -p $LogDir # ensure the directory to store fsl_sub logfiles exists
+    queuing_command="${FSLDIR}/bin/fsl_sub ${QUEUE} -l $LogDir"
+fi
+
+
+########################################## INPUTS ##########################################
+
+#Scripts called by this script do assume they run on the outputs of the FreeSurfer Pipeline
+
+######################################### DO WORK ##########################################
+
+
+for Subject in $Subjlist ; do
+  echo $Subject
+
+  #Input Variables
+  SurfaceAtlasDIR="${HCPPIPEDIR_Templates}/standard_mesh_atlases"
+  GrayordinatesSpaceDIR="${HCPPIPEDIR_Templates}/91282_Greyordinates"
+  GrayordinatesResolutions="2" #Usually 2mm, if multiple delimit with @, must already exist in templates dir
+  HighResMesh="164" #Usually 164k vertices
+  LowResMeshes="32" #Usually 32k vertices, if multiple delimit with @, must already exist in templates dir
+  SubcorticalGrayLabels="${HCPPIPEDIR_Config}/FreeSurferSubcorticalLabelTableLut.txt"
+  FreeSurferLabels="${HCPPIPEDIR_Config}/FreeSurferAllLut.txt"
+  ReferenceMyelinMaps="${HCPPIPEDIR_Templates}/standard_mesh_atlases/Conte69.MyelinMap_BC.164k_fs_LR.dscalar.nii"
+  # RegName="MSMSulc" #MSMSulc is recommended, if binary is not available use FS (FreeSurfer)
+  RegName="FS"
+
+  ${queuing_command} $ThisFolder/PostFreeSurferPipeline_fromacpc.sh \
+      --path="$StudyFolder" \
+      --subject="$Subject" \
+      --surfatlasdir="$SurfaceAtlasDIR" \
+      --grayordinatesdir="$GrayordinatesSpaceDIR" \
+      --grayordinatesres="$GrayordinatesResolutions" \
+      --hiresmesh="$HighResMesh" \
+      --lowresmesh="$LowResMeshes" \
+      --subcortgraylabels="$SubcorticalGrayLabels" \
+      --freesurferlabels="$FreeSurferLabels" \
+      --refmyelinmaps="$ReferenceMyelinMaps" \
+      --regname="$RegName" \
+      --printcom=$PRINTCOM
+
+  # The following lines are used for interactive debugging to set the positional parameters: $1 $2 $3 ...
+
+   echo "set -- --path="$StudyFolder" \
+      --subject="$Subject" \
+      --surfatlasdir="$SurfaceAtlasDIR" \
+      --grayordinatesdir="$GrayordinatesSpaceDIR" \
+      --grayordinatesres="$GrayordinatesResolutions" \
+      --hiresmesh="$HighResMesh" \
+      --lowresmesh="$LowResMeshes" \
+      --subcortgraylabels="$SubcorticalGrayLabels" \
+      --freesurferlabels="$FreeSurferLabels" \
+      --refmyelinmaps="$ReferenceMyelinMaps" \
+      --regname="$RegName" \
+      --printcom=$PRINTCOM"
+
+   echo ". ${EnvironmentScript}"
+done
---
+++ in_vivo/readme.txt
@@ -0,0 +1,13 @@
+HCPscripts
+--------------------------------------------------
+MrCat uses some scripts from the HCP pipeline. For ease of distribution, and to prevent people from cloning the full HCP pipeline repository if they only need a single script, we have included a copy of the most used scripts here.
+
+
+SCRIPTS
+-------
+Correct spatial bias based on robust iterations of FAST
+	RobustBiasCorr.sh:				
+HACK to re-run HCP data from acpc:
+	CreateMyelingMaps_fromacpc.sh
+	PostFreeSurferPipeline_fromacpc.sh
+	PostFreeSurferPipelineBatch_fromacpc.sh
